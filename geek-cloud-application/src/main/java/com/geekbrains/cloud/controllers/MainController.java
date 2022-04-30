671be950918daeb7a7f39dc24e090f34a42c9f66
Maria Kuzmina <bigcat82@gmail.com>
2022-04-30 13:06:00 +0300

    Initial commit

---
+++ geek-cloud-application/src/main/java/com/geekbrains/cloud/controllers/MainController.java
@@ -0,0 +1,95 @@
+package com.geekbrains.cloud.controllers;
+
+import com.geekbrains.cloud.network.Net;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    private static final String LOCAL_FILE_PATH = "files";
+    private Net net;
+
+
+    @FXML
+    public ListView<String> serverFiles;
+    @FXML
+    public ListView<String> localFiles;
+    @FXML
+    public TextField input;
+
+    private void read() {
+        while (true) {
+            try {
+                if (Thread.currentThread().isInterrupted()) {
+                    return;
+                }
+                String command = net.readUtf();
+                if (command.equals("#list#")) {
+                    readServerListFiles();
+                }
+                if (command.equals("#ready#")) {
+                    String file = localFiles.getSelectionModel().getSelectedItem();
+                    net.sendFile(new File(LOCAL_FILE_PATH + '/' + file));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                net.closeConnection();
+                break;
+            }
+        }
+    }
+
+    private void readServerListFiles() {
+        try {
+            Platform.runLater(() ->serverFiles.getItems().clear());
+            Long filesCount = net.readLong();
+            for (int i = 0; i < filesCount; i++) {
+                String fileName = net.readUtf();
+                Platform.runLater(() -> serverFiles.getItems().addAll(fileName));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readLocalFilesList(String localFilePath) {
+        File dir = new File(localFilePath);
+        String[] localFilesList = dir.list();
+
+        localFiles.getItems().addAll(localFilesList);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            net = Net.getINSTANCE();
+            readLocalFilesList(LOCAL_FILE_PATH);
+            Thread readThread = new Thread(this::read);
+            readThread.setDaemon(true);
+            readThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void mouseClicked(MouseEvent mouseEvent) {
+        if (mouseEvent.getClickCount() == 2) {
+            try {
+                net.sendUtf("#file#");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ geek-cloud-server/src/main/java/com/geekbrains/cloud/FileMessageHandler.java
@@ -0,0 +1,76 @@
+package com.geekbrains.cloud;
+
+import java.io.*;
+import java.net.Socket;
+
+public class FileMessageHandler implements Runnable {
+
+    private final File dir;
+    private final DataInputStream inputStream;
+    private final DataOutputStream outputStream;
+
+    private static final String FILE_PATH = "server-files";
+
+    public FileMessageHandler(Socket socket) throws IOException {
+        inputStream = new DataInputStream(socket.getInputStream());
+        outputStream = new DataOutputStream(socket.getOutputStream());
+        System.out.println("Client accepted");
+
+        dir = new File(FILE_PATH);
+        sendFilesList();
+    }
+
+    private void sendFilesList() throws IOException {
+        String[] files = dir.list();
+
+        outputStream.writeUTF("#list#");
+        outputStream.writeLong(files.length);
+
+        for (String file : files) {
+            outputStream.writeUTF(file);
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                String utf = inputStream.readUTF();
+                if (utf.equals("#file#")) {
+                    outputStream.writeUTF("#ready#");
+
+                    String fileName = inputStream.readUTF();
+
+                    getFile(fileName);
+
+                    outputStream.writeUTF("#test#");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getFile(String fileName) throws IOException {
+        System.out.println("ready to receive file");
+        try (FileOutputStream fileOutputStream = new FileOutputStream(FILE_PATH+"/" + fileName)) {
+            long fileLength = inputStream.readLong();
+            byte[] buffer = new byte[1024];
+            int dataLength;
+            while ((dataLength = inputStream.read(buffer)) > 0) {
+                System.out.println(dataLength);
+                fileOutputStream.write(buffer,0, dataLength);
+                fileLength -= dataLength;
+                if (fileLength <= 0) {
+                    break;
+                }
+            }
+            fileOutputStream.flush();
+
+            System.out.println("file received");
+            sendFilesList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
